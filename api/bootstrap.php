3f408af3be15ac22fe9141e9755d8269da39ff98
Alyanna Arcebido <alyanna.arcebido@gmail.com>
2026-09-07 14:39:30 +0800

    Fix PHP session and Projects page

--- api/bootstrap.php
+++ api/bootstrap.php
@@ -1,4 +1,9 @@
 <?php
+header('Access-Control-Allow-Origin: http://127.0.0.1:5501');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Credentials: true');
+
 header('Content-Type: application/json; charset=utf-8');
 require_once __DIR__ . '/../config/db.php';
 if (session_status() === PHP_SESSION_NONE) session_start();
